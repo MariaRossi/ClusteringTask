b86ff848cfeff41f1238cfa7405384d7f3a86633
Maria Evgenia Rossi <maria.rossi@inria.fr>
2018-05-21 18:48:24 +0200

    Added function to read json file

--- clustering_test.py
+++ clustering_test.py
@@ -5,6 +5,21 @@ from sklearn import cluster
 import matplotlib.pyplot as plt
 
 
+def read_json_data(input_file):
+
+	"""
+	Read data from json file.
+
+	Takes as input the json file and returns a Python list of all the entries.
+
+	"""
+
+	with open(input_file) as json_data:
+
+		data = json.load(json_data)
+	
+	return data
+
 
 if __name__ == "__main__":
 
